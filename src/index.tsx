2c193fa72621f08b1659fc4622969f729e02ae86
David Totraev <totraev@gmail.com>
2024-11-23 16:59:35 +0500

    feat: export core type

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import "./index.css";
 
+export { WalletProvider } from "@/components/WalletProvider";
 export { useChainConnector } from "@/hooks/useChainConnector";
 export { useAppState } from "@/state/state";
-export { WalletProvider } from "@/components/WalletProvider";
 export * from "@/state/types";
+
+export * from "@/core/wallets/btc/BTCProvider";
+export * from "@/core/wallets/bbn/BBNProvider";
+export * from "@/core/types";
