b98533e0718dd5a3708eef32a7ad86c8d81422ba
Tong Li <tongli0809@gmail.com>
2024-11-30 22:41:07 +1100

    do not rollup @cosmjs/stargate because it does not work with pre-rendering in NextJs

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,14 @@ export default defineConfig({
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom", "react/jsx-runtime", "tailwind-merge", "@babylonlabs-io/bbn-core-ui"],
+      external: [
+        "react",
+        "react-dom",
+        "react/jsx-runtime",
+        "tailwind-merge",
+        "@babylonlabs-io/bbn-core-ui",
+        "@cosmjs/stargate",
+      ],
       output: {
         sourcemapExcludeSources: true,
       },
