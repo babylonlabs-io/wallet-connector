7dcb483a330a8e1eae679aa9170c88f2d34e2d83
Govard Barkhatov <gbarkhatov@gmail.com>
2024-12-12 01:18:41 +0200

    bbn injectable

---
+++ src/core/wallets/bbn/injectable/index.ts
@@ -0,0 +1,15 @@
+import { Network, type BBNConfig, type WalletMetadata } from "@/core/types";
+
+import { BBNProvider } from "../BBNProvider";
+
+const metadata: WalletMetadata<BBNProvider, BBNConfig> = {
+  id: "injectable",
+  name: (wallet) => wallet.getWalletProviderName?.(),
+  icon: (wallet) => wallet.getWalletProviderIcon?.(),
+  docs: "",
+  wallet: "bbnwallet",
+  createProvider: (wallet) => wallet,
+  networks: [Network.MAINNET, Network.SIGNET],
+};
+
+export default metadata;
