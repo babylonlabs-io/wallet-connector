07245d0502d8b21ec787bc1d22b474e5116f9bb6
Govard Barkhatov <gbarkhatov@gmail.com>
2024-12-11 13:31:47 +0200

    feat: Node polyfills (#111)
    * node polyfills
    
    * changeset

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import path from "node:path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
   plugins: [
@@ -12,6 +13,7 @@ export default defineConfig({
       include: ["src"],
       exclude: ["src/**/*.stories.tsx"],
     }),
+    nodePolyfills(),
   ],
   build: {
     outDir: "dist",
