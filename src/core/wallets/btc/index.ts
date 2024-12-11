7517ed535d4464e3dd5651887fb24ce8d9d80af3
Govard Barkhatov <gbarkhatov@gmail.com>
2024-12-11 10:54:39 +0200

    feat: Bitget wallet (#103)
    * bitget wallet
    
    * comment
    
    * rm duplicate imports

--- src/core/wallets/btc/index.ts
+++ src/core/wallets/btc/index.ts
@@ -2,6 +2,7 @@ import type { BTCConfig, ChainMetadata } from "@/core/types";
 
 import { BTCProvider } from "./BTCProvider";
 import icon from "./bitcoin.png";
+import bitget from "./bitget";
 import injectable from "./injectable";
 import okx from "./okx";
 import onekey from "./onekey";
@@ -10,7 +11,7 @@ const metadata: ChainMetadata<"BTC", BTCProvider, BTCConfig> = {
   chain: "BTC",
   name: "Bitcoin",
   icon,
-  wallets: [injectable, okx, onekey],
+  wallets: [injectable, okx, onekey, bitget],
 };
 
 export default metadata;
