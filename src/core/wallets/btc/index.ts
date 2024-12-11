d3fe4a9ba0f6a24c3930af0cb60bbea644edf7ab
Govard Barkhatov <gbarkhatov@gmail.com>
2024-12-11 10:52:13 +0200

    rm duplicate imports

--- src/core/wallets/btc/index.ts
+++ src/core/wallets/btc/index.ts
@@ -2,13 +2,10 @@ import type { BTCConfig, ChainMetadata } from "@/core/types";
 
 import { BTCProvider } from "./BTCProvider";
 import icon from "./bitcoin.png";
+import bitget from "./bitget";
 import injectable from "./injectable";
 import okx from "./okx";
 import onekey from "./onekey";
-import bitget from "./bitget";
-
-import type { ChainMetadata, BTCConfig } from "@/core/types";
-
 
 const metadata: ChainMetadata<"BTC", BTCProvider, BTCConfig> = {
   chain: "BTC",
