9fe039ca00e3a67ff99bbc9818854f15965e8abf
Govard Barkhatov <gbarkhatov@gmail.com>
2024-12-11 10:47:28 +0200

    feat: OneKey wallet (#102)
    * changeset
    
    * add onekey
    
    * comments
    
    * extract issue

--- src/core/wallets/btc/index.ts
+++ src/core/wallets/btc/index.ts
@@ -4,12 +4,13 @@ import { BTCProvider } from "./BTCProvider";
 import icon from "./bitcoin.png";
 import injectable from "./injectable";
 import okx from "./okx";
+import onekey from "./onekey";
 
 const metadata: ChainMetadata<"BTC", BTCProvider, BTCConfig> = {
   chain: "BTC",
   name: "Bitcoin",
   icon,
-  wallets: [injectable, okx],
+  wallets: [injectable, okx, onekey],
 };
 
 export default metadata;
