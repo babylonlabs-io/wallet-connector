91f1596e89c7f62bf5c94d4e62dada7ff8abec7b
Govard Barkhatov <gbarkhatov@gmail.com>
2024-12-13 15:01:48 +0200

    feat: BBN injectable (#118)
    * bbn injectable
    
    * changeset
    
    * Update src/core/wallets/bbn/injectable/index.ts
    
    Co-authored-by: David Totrashvili <8580261+totraev@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: David Totrashvili <8580261+totraev@users.noreply.github.com>

--- src/core/wallets/bbn/index.ts
+++ src/core/wallets/bbn/index.ts
@@ -2,13 +2,14 @@ import { BBNConfig, ChainMetadata } from "@/core/types";
 
 import { BBNProvider } from "./BBNProvider";
 import icon from "./babylon.jpeg";
+import injectable from "./injectable";
 import keplr from "./keplr";
 
 const metadata: ChainMetadata<"BBN", BBNProvider, BBNConfig> = {
   chain: "BBN",
   name: "Babylon Chain",
   icon,
-  wallets: [keplr],
+  wallets: [injectable, keplr],
 };
 
 export default metadata;
---
+++ src/core/wallets/bbn/injectable/index.ts
@@ -0,0 +1,16 @@
+import { Network, type BBNConfig, type WalletMetadata } from "@/core/types";
+
+import { BBNProvider } from "../BBNProvider";
+
+const metadata: WalletMetadata<BBNProvider, BBNConfig> = {
+  id: "injectable",
+  name: (wallet) => wallet.getWalletProviderName?.(),
+  icon: (wallet) => wallet.getWalletProviderIcon?.(),
+  docs: "",
+  wallet: "bbnwallet",
+  createProvider: (wallet) => wallet,
+  networks: [Network.MAINNET, Network.SIGNET],
+  label: 'Injectable'
+};
+
+export default metadata;
