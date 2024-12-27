41f4ab2a13f1d8f3bbf3d12aa73e4d145415919d
wjrjerome <wjrjerome@babylonchain.io>
2024-12-27 15:42:35 +1100

    chore: update inscription text

--- src/components/Inscriptions/index.tsx
+++ src/components/Inscriptions/index.tsx
@@ -11,34 +11,50 @@ export interface Props {
   config?: BTCConfig;
 }
 
-export function Inscriptions({ className, onSubmit }: Props) {
+export function Inscriptions({ className, onSubmit, config }: Props) {
   const [lockInscriptions = true, toggleInscriptions] = useState<boolean | undefined>();
   const [showAgain = true, toggleShowAgain] = useState<boolean | undefined>();
 
+  if (!config) return null;
+
+  const { coinName } = config;
+
   return (
     <div className={twMerge("b-mb-8 b-flex b-flex-1 b-flex-col", className)}>
-      <DialogHeader title="Bitcoin Inscriptions" className="b-mb-8">
-        <Text className="b-mb-6">
-          This staking interface attempts to detect bitcoin ordinals, NFTs, Runes, and other inscriptions
-          (“Inscriptions”) within the Unspent Transaction Outputs (“UTXOs”) in your wallet. If you stake bitcoin with
-          Inscriptions, those UTXOs may be spent on staking, unbonding, or withdrawal fees, which will cause you to lose
-          those Inscriptions permanently. This interface will not detect all Inscriptions.
-        </Text>
-
-        <Text>Chose one: (you can change this later)</Text>
+      <DialogHeader title={`Manage ${coinName} Inscriptions`} className="b-mb-8">
+        <br />
+        <div className="b-mb-6">
+          <Text>
+            By default, we will not use {coinName} that contains Inscriptions - such as Ordinals, NFTs, or Runes - in
+            your stakeable balance. This helps prevent any accidental loss of your Inscriptions due to staking,
+            unbonding, or withdrawal fees.
+          </Text>
+          <br />
+          <Text>
+            If you would like to include {coinName} with Inscriptions in your stakeable balance, please select the
+            option below.
+          </Text>
+        </div>
       </DialogHeader>
-
       <DialogBody>
         <form action="">
           <FieldControl
-            label="Lock bitcoin UTXOs with detected Inscriptions so they will not be spent."
+            label={
+              <>
+                <strong>Do not use</strong> {coinName} with Inscriptions for staking. (Recommended)
+              </>
+            }
             className="b-mb-8"
           >
             <Radio name="inscriptions" checked={lockInscriptions} onChange={() => toggleInscriptions(true)} />
           </FieldControl>
 
           <FieldControl
-            label="Unlock bitcoin UTXOs with detected Inscriptions in my stakable balance. I understand and agree that doing so can cause the complete and permanent loss of Inscriptions and that I am solely liable and responsible for their loss."
+            label={
+              <>
+                <strong>Use</strong> {coinName} with Inscriptions in my stakable balance.
+              </>
+            }
             className="b-mb-8"
           >
             <Radio name="inscriptions" checked={!lockInscriptions} onChange={() => toggleInscriptions(false)} />
