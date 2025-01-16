ea7dd469e44973809ca409913ebb3d6be083bf91
Govard Barkhatov <gbarkhatov@gmail.com>
2025-01-16 12:08:54 +0200

    add leap wallet (#190)

--- src/core/wallets/bbn/index.ts
+++ src/core/wallets/bbn/index.ts
@@ -3,12 +3,13 @@ import { BBNConfig, ChainMetadata, IBBNProvider } from "@/core/types";
 import icon from "./babylon.jpeg";
 import injectable from "./injectable";
 import keplr from "./keplr";
+import leap from "./leap";
 
 const metadata: ChainMetadata<"BBN", IBBNProvider, BBNConfig> = {
   chain: "BBN",
   name: "Babylon Chain",
   icon,
-  wallets: [injectable, keplr],
+  wallets: [injectable, keplr, leap],
 };
 
 export default metadata;
