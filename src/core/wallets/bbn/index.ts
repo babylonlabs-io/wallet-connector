1b99e13e9ed15248b352280eb52b32866fc29ed1
Govard Barkhatov <gbarkhatov@gmail.com>
2025-01-16 12:15:28 +0200

    feat: OKX - Cosmos (#192)
    * add okx babylon wallet
    
    * babylon testnet
    
    * update testnet low

--- src/core/wallets/bbn/index.ts
+++ src/core/wallets/bbn/index.ts
@@ -4,12 +4,13 @@ import icon from "./babylon.jpeg";
 import injectable from "./injectable";
 import keplr from "./keplr";
 import leap from "./leap";
+import okx from "./okx";
 
 const metadata: ChainMetadata<"BBN", IBBNProvider, BBNConfig> = {
   chain: "BBN",
   name: "Babylon Chain",
   icon,
-  wallets: [injectable, keplr, leap],
+  wallets: [injectable, keplr, leap, okx],
 };
 
 export default metadata;
