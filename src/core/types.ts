6000936861e02c27e17b8f864e7f3f1a8d750d4b
Jeremy <168515712+jeremy-babylonlabs@users.noreply.github.com>
2025-02-05 11:04:22 +0800

    add canary network (#222)
    * add canary network
    
    * resolve comments

--- src/core/types.ts
+++ src/core/types.ts
@@ -35,6 +35,7 @@ export interface InscriptionIdentifier {
 // supported networks
 export enum Network {
   MAINNET = "mainnet",
+  CANARY = "canary",
   TESTNET = "testnet",
   SIGNET = "signet",
 }
