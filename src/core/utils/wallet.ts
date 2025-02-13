f40e6fef04bed49e89f587242cf6f1ebde2d6667
David Totrashvili <8580261+0xDazzer@users.noreply.github.com>
2025-02-13 23:08:45 +0500

    feat: validate address and pk (#231)
    * feat: validate address and pk
    
    * fix: review issues

--- src/core/utils/wallet.ts
+++ src/core/utils/wallet.ts
@@ -1,32 +1,106 @@
-import { networks } from "bitcoinjs-lib";
+import { networks, payments } from "bitcoinjs-lib";
+import { toXOnly } from "bitcoinjs-lib/src/psbt/bip371";
 
 import { Network } from "@/core/types";
+export const COMPRESSED_PUBLIC_KEY_HEX_LENGTH = 66;
+
+const NETWORKS = {
+  [Network.MAINNET]: {
+    name: "Mainnet",
+    config: networks.bitcoin,
+    addressPrefix: {
+      common: "bc1",
+      nativeSegWit: "bc1q",
+      taproot: "bc1p",
+    },
+  },
+  [Network.CANARY]: {
+    name: "Canary",
+    config: networks.bitcoin,
+    addressPrefix: {
+      common: "bc1",
+      nativeSegWit: "bc1q",
+      taproot: "bc1p",
+    },
+  },
+  [Network.TESTNET]: {
+    name: "Testnet",
+    config: networks.testnet,
+    addressPrefix: {
+      common: "tb1",
+      nativeSegWit: "tb1q",
+      taproot: "tb1p",
+    },
+  },
+  [Network.SIGNET]: {
+    name: "Signet",
+    config: networks.testnet,
+    addressPrefix: {
+      common: "tb1",
+      nativeSegWit: "tb1q",
+      taproot: "tb1p",
+    },
+  },
+};
+
+export const getTaprootAddress = (publicKey: string, network: Network) => {
+  if (publicKey.length == COMPRESSED_PUBLIC_KEY_HEX_LENGTH) {
+    publicKey = publicKey.slice(2);
+  }
+
+  const internalPubkey = Buffer.from(publicKey, "hex");
+  const { address, output: scriptPubKey } = payments.p2tr({
+    internalPubkey: toXOnly(internalPubkey),
+    network: NETWORKS[network].config,
+  });
+
+  if (!address || !scriptPubKey) {
+    throw new Error("Failed to generate taproot address or script from public key");
+  }
+
+  return address;
+};
+
+export const getNativeSegwitAddress = (publicKey: string, network: Network) => {
+  if (publicKey.length !== COMPRESSED_PUBLIC_KEY_HEX_LENGTH) {
+    throw new Error("Invalid public key length for generating native segwit address");
+  }
+
+  const internalPubkey = Buffer.from(publicKey, "hex");
+  const { address, output: scriptPubKey } = payments.p2wpkh({
+    pubkey: internalPubkey,
+    network: NETWORKS[network].config,
+  });
+
+  if (!address || !scriptPubKey) {
+    throw new Error("Failed to generate native segwit address or script from public key");
+  }
+
+  return address;
+};
+
+export function validateAddressWithPK(address: string, publicKey: string, network: Network) {
+  if (address.startsWith(NETWORKS[network].addressPrefix.taproot)) {
+    return address === getTaprootAddress(publicKey, network);
+  }
+
+  if (address.startsWith(NETWORKS[network].addressPrefix.nativeSegWit)) {
+    return address === getNativeSegwitAddress(publicKey, network);
+  }
+
+  return false;
+}
 
 export function validateAddress(network: Network, address: string): void {
-  if (network === Network.MAINNET && !address.startsWith("bc1")) {
-    throw new Error("Incorrect address prefix for Mainnet. Expected address to start with 'bc1'.");
-  }
-  if (network === Network.CANARY && !address.startsWith("bc1")) {
-    throw new Error("Incorrect address prefix for Canary. Expected address to start with 'bc1'.");
-  }
-  if ((network === Network.TESTNET || network === Network.SIGNET) && !address.startsWith("tb1")) {
-    throw new Error("Incorrect address prefix for Testnet/Signet. Expected address to start with 'tb1'.");
+  const { addressPrefix, name } = NETWORKS[network];
+
+  if (!(network in NETWORKS)) {
+    throw new Error(`Unsupported network: ${network}. Please provide a valid network.`);
   }
 
-  if (![Network.MAINNET, Network.SIGNET, Network.TESTNET, Network.CANARY].includes(network)) {
-    throw new Error(`Unsupported network: ${network}. Please provide a valid network.`);
+  if (!address.startsWith(addressPrefix.common)) {
+    throw new Error(`Incorrect address prefix for ${name}. Expected address to start with '${addressPrefix}'.`);
   }
 }
 
-export const toNetwork = (network: Network): networks.Network => {
-  switch (network) {
-    case Network.MAINNET:
-    case Network.CANARY:
-      return networks.bitcoin;
-    case Network.TESTNET:
-    case Network.SIGNET:
-      return networks.testnet;
-    default:
-      throw new Error("Unsupported network");
-  }
-};
+export const toNetwork = (network: Network): networks.Network => NETWORKS[network].config;
