7639df5dd436fb2a34f80815f34a9d984c7be6f1
Govard Barkhatov <gbarkhatov@gmail.com>
2025-03-04 16:13:37 +0200

    feat: Offline Signer Auto (#259)
    * use offline signer auto
    
    * changeset
    
    * rm clog
    
    * offline signer
    
    * either
    
    * readme

--- src/core/wallets/bbn/leap/provider.ts
+++ src/core/wallets/bbn/leap/provider.ts
@@ -95,4 +95,15 @@ export class LeapProvider implements IBBNProvider {
       throw new Error("Failed to get offline signer");
     }
   }
+
+  async getOfflineSignerAuto(): Promise<OfflineAminoSigner | OfflineDirectSigner> {
+    if (!this.wallet) throw new Error("Leap extension not found");
+    if (!this.chainId) throw new Error("Chain ID is not initialized");
+
+    try {
+      return this.wallet.getOfflineSignerAuto(this.chainId);
+    } catch {
+      throw new Error("Failed to get offline signer auto");
+    }
+  }
 }
