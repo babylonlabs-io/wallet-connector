d655179e18f30d846c58005f78ebbc4e7b04ce9b
Jeremy <168515712+jeremy-babylonlabs@users.noreply.github.com>
2025-05-08 17:06:11 +0800

    add wallet error (#376)
    * add wallet error
    
    * resolve comments

---
+++ src/error/index.ts
@@ -0,0 +1,22 @@
+interface ErrorParams {
+  code: string;
+  message: string;
+  wallet: string;
+  version?: string;
+  chainId?: string;
+}
+
+export class WalletError extends Error {
+  readonly code: string;
+  readonly wallet: string;
+  readonly version?: string;
+  readonly chainId?: string;
+
+  constructor({ code, message, wallet, version, chainId }: ErrorParams, options?: ErrorOptions) {
+    super(message, options);
+    this.code = code;
+    this.wallet = wallet;
+    this.version = version;
+    this.chainId = chainId;
+  }
+}
